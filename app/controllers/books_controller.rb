75864d8e62de0899abd19026ed7f0991eb12ba4f
Yoichi-Maribao <szk.m.yo1@gmail.com>
2021-09-09 14:31:58 +0000

    一週間以内のいいね順

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
   def index
+    to = Time.current.at_end_of_day
+    from = (to - 6.day).at_beginning_of_day
     @user = current_user
-    @books = Book.includes(:favorites).sort {|a,b| b.favorites.size <=> a.favorites.size }
+    @books = Book.includes(:favorites).sort {|a,b| b.favorites.where(created_at: from...to).size <=> a.favorites.where(created_at: from...to).size }
     @book = Book.new
   end
 
