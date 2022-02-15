face1640d929c60507211da9e5a6751b819bcf53
EC2 Default User <ec2-user@ip-172-31-2-153.us-east-2.compute.internal>
2022-02-15 17:19:35 +0000

    taa

--- config/puma.rb
+++ config/puma.rb
@@ -43,8 +43,9 @@ if Rails.env.production?
   pidfile File.join(rails_root, 'tmp', 'pids', 'puma.pid')
   state_path File.join(rails_root, 'tmp', 'pids', 'puma.state')
   stdout_redirect(
-    File.join(rails_root, 'log', 'puma.log')
-    File.join(rails_root, 'log', 'puma-error.log')
+    File.join(rails_root, 'log', 'puma.log'),
+    File.join(rails_root, 'log', 'puma-error.log'),
+    true
   )
-  demonize
+  daemonize
 end
